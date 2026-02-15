9a8892661462f10a5d8c83b3cd3a5b695ee516a4
Alexey Bratanchuk <bratanchuk.al.work@gmail.com>
2026-02-15 09:36:26 +0300

    AIT-1 [upd] transcription: draft giga_am

--- src/transcription/giga_am/main.py
+++ src/transcription/giga_am/main.py
@@ -7,18 +7,73 @@ import gigaam
 
 
 #
-def run_giga_am(audio_path):
+import hydra
+import torch
+from gigaam import GigaAMASR
+from gigaam.model import LONGFORM_THRESHOLD
+
+from segmentation import transcribe
+
+
+class ExGigaAMASR(GigaAMASR):
+    def __init__(self, model: GigaAMASR):
+        super().__init__(model.cfg)
+        self.head = hydra.utils.instantiate(self.cfg.head)
+        self.decoding = hydra.utils.instantiate(self.cfg.decoding)
+
+    @torch.inference_mode()
+    def transcribe(self, wav_file: str) -> str:
+        """
+        Transcribes a short audio file into text.
+        """
+        wav, length = self.prepare_wav(wav_file)
+        if length.item() > LONGFORM_THRESHOLD:
+            raise ValueError("Too long wav file, use 'transcribe_longform' method.")
+
+        encoded, encoded_len = self.forward(wav, length)
+        return self.decoding.decode(self.head, encoded, encoded_len)[0]
+
+    @property
+    def device(self) -> torch.device:
+        return super()._device
+
+def run_giga_am(audio_path: str):
     # Audio embeddings
-    model_name = "v3_ssl"  # Options: `v1_ssl`, `v2_ssl`, `v3_ssl`
-    model = gigaam.load_model(model_name)
-    embedding, _ = model.embed_audio(audio_path)
-    print(embedding)
+    # model_name = "v3_ssl"  # Options: `v1_ssl`, `v2_ssl`, `v3_ssl`
+    # model = gigaam.load_model(model_name)
+    # embedding, _ = model.embed_audio(audio_path)
+    # print(embedding)
 
     # ASR
     model_name = "v3_e2e_rnnt"  # Options: any model version with suffix `_ctc` or `_rnnt`
     model = gigaam.load_model(model_name)
+    wav, length = model.prepare_wav(audio_path)
+    encoded, encoded_len = model.forward(wav, length)
+
+    transcribe(ExGigaAMASR(model),
+               audio_path,
+               )
+
+    # wav, length = model.prepare_wav(audio_path)
+    # if length.item() > LONGFORM_THRESHOLD:
+    #     pass
+    # encoded, encoded_len = model.forward(wav, length)
+    # a  = model.decoding.decode(model.head, encoded, encoded_len)[0]
+
+    # utterances = model.transcribe_longform(audio_path)
+    # for utt in utterances:
+    #     transcription, (start, end) = utt["transcription"], utt["boundaries"]
+    #     print(f"[{gigaam.format_time(start)} - {gigaam.format_time(end)}]: {transcription}")
+
+    # model_name = "v3_e2e_rnnt"  # Options: any model version with suffix `_ctc` or `_rnnt`
+    # model = gigaam.load_model(model_name)
     transcription = model.transcribe(audio_path)
-    print(transcription)
+    # print(transcription)
+    #
+    # model_name = "v3_e2e_ctc"  # Options: any model version with suffix `_ctc` or `_rnnt`
+    # model = gigaam.load_model(model_name)
+    # transcription = model.transcribe(audio_path)
+    # print(transcription)
 
     # Emotion recognition
     model = gigaam.load_model("emo")
